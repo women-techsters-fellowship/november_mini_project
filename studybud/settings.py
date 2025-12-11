f4b1435f648c33f979db5baf7110118255318ea0
jemimahrimdan <jemimahrimdan0@gmail.com>
2025-12-11 21:23:27 +0100

    Updated Dockerfile, settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -130,12 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # STATIC_ROOT =
